5e036f551ebed67ac94abeec014534633434c159
jyoon75 <jjyoon075@gmail.com>
2025-01-21 06:26:12 +0900

    이미지 접근 권한 설정, 로그인 액티비티 생성, 캘린더 작업 중

--- app/src/main/java/com/example/meonail/MainActivity.kt
+++ app/src/main/java/com/example/meonail/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.meonail
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,6 +17,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.meonail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
 
     private lateinit var binding: ActivityMainBinding
 
@@ -34,11 +41,44 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
+
         // 후기 작성 버튼 클릭 이벤트
         binding.btnRecordRegist.setOnClickListener {
-            // RecordRegistActivity로 이동
-            val intent = Intent(this, RecordRegistActivity::class.java)
-            startActivity(intent)
+            if(checkPermission()){ // 사진 권한 참일 때
+                // RecordRegistActivity로 이동
+                val intent = Intent(this, RecordRegistActivity::class.java)
+                startActivity(intent)
+            }
         }
+    }
+
+    private fun checkPermission(): Boolean {
+        // 빌드 버전에 따라 권한 체크
+        val readImagePermissioin = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            android.Manifest.permission.READ_MEDIA_IMAGES
+        else android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+        if (ContextCompat.checkSelfPermission(this, readImagePermissioin) != PackageManager.PERMISSION_GRANTED){
+            // 권한이 부여되지 않았다면
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, readImagePermissioin)){
+                // 이미 거부된 적이 있다면
+                var dlg = AlertDialog.Builder(this)
+                dlg.setTitle("미디어 접근 권한")
+                dlg.setMessage("사진 정보를 얻기 위해서는 외부 저장소 권한이 필수로 필요합니다.")
+                dlg.setPositiveButton("확인") {dialog, which -> ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(readImagePermissioin),
+                    REQUEST_READ_EXTERNAL_STORAGE)}
+                dlg.setNegativeButton("취소", null)
+                dlg.show()
+            } else {
+                ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(readImagePermissioin),
+                    REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            // 권한이 부여되었다면 참 리턴
+            return true
+        }
+        return false
     }
 }
